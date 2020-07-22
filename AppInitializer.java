6f0e0c0bc0da1ac9f99375aeb953e39b2fda2868
tharinduperera <tharindudananjaya199818@gmail.com>
2020-07-22 11:37:08 +0530

    Update AppInitializer class

--- AppInitializer.java
+++ AppInitializer.java
@@ -2,7 +2,9 @@ public class AppInitializer{
     public static void main(String args[]){
         Student s1 = new Student(1,"Appu");
         System.out.println(s1);
+        for(int i=0;i<5;i++){
         Student s2 = new Student(1,"Sanoj");
         System.out.println(s2);
+        }
     }
 }
