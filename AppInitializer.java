d288cb907b52d40f3ca93e26baf5a965356a0e38
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2020-07-22 11:09:55 +0530

    Initial commit

---
+++ AppInitializer.java
@@ -0,0 +1,8 @@
+public class AppInitializer{
+    public static void main(String args[]){
+        Student s1 = new Student(1,"Appu");
+        System.out.println(s1);
+        Student s2 = new Student(1,"Sanoj");
+        System.out.println(s2);
+    }
+}
